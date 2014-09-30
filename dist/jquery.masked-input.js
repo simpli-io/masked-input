b879c9065f07c590df50e5cc469f59c25026e7f3
Andrew Walton <andrew.p.walton12@gmail.com>
2014-09-30 13:09:19 -0400

    Fixed formatting of jQuery plugin.

--- dist/jquery.masked-input.js
+++ dist/jquery.masked-input.js
@@ -6,52 +6,41 @@
  *  Made by Zeno Rocha
  *  Under MIT License
  */
-// the semi-colon before function invocation is a safety net against concatenated
-// scripts and/or other plugins which may not be closed properly.
-;(function ( $, window, document, undefined ) {
+(function ($, window, document, undefined) {
 
-		// Create the defaults once
-		var pluginName = "maskedInput",
-			defaults = {
-				propertyName: "value"
-			};
+    // Create the defaults once
+    var pluginName = "maskedInput",
+        defaults = {
+            propertyName: "value"
+        };
 
-		// The actual plugin constructor
-		function Plugin ( element, options ) {
-			this.element = element;
-			this.settings = $.extend( {}, defaults, options );
-			this._defaults = defaults;
-			this._name = pluginName;
-			this.init();
-		}
+    // The actual plugin constructor
+    function MaskedInput(element, options) {
+        this.element = element;
+        this.settings = $.extend({}, defaults, options);
+        this._defaults = defaults;
+        this._name = pluginName;
+        this.init();
+    }
 
-		// Avoid Plugin.prototype conflicts
-		$.extend(Plugin.prototype, {
-				init: function () {
-						// Place initialization logic here
-						// You already have access to the DOM element and
-						// the options via the instance, e.g. this.element
-						// and this.settings
-						// you can add more functions like the one below and
-						// call them like so: this.yourOtherFunction(this.element, this.settings).
-						console.log("xD");
-				},
-				yourOtherFunction: function () {
-						// some logic
-				}
-		});
+    // Avoid MaskedInput.prototype conflicts
+    $.extend(MaskedInput.prototype, {
+        init: function () {
+            console.log("Initializing simpli masked input.");
+        }
+    });
 
-		// A really lightweight plugin wrapper around the constructor,
-		// preventing against multiple instantiations
-		$.fn[ pluginName ] = function ( options ) {
-				this.each(function() {
-						if ( !$.data( this, "plugin_" + pluginName ) ) {
-								$.data( this, "plugin_" + pluginName, new Plugin( this, options ) );
-						}
-				});
+    // A really lightweight plugin wrapper around the constructor,
+    // preventing against multiple instantiations
+    $.fn[pluginName] = function (options) {
+        this.each(function () {
+            if (!$.data(this, "simpli" + pluginName)) {
+                $.data(this, "simpli" + pluginName, new MaskedInput(this, options));
+            }
+        });
 
-				// chain jQuery functions
-				return this;
-		};
+        // chain jQuery functions
+        return this;
+    };
 
-})( jQuery, window, document );
+})(jQuery, window, document);
--- src/jquery.masked-input.js
+++ src/jquery.masked-input.js
@@ -1,40 +1,38 @@
-// the semi-colon before function invocation is a safety net against concatenated
-// scripts and/or other plugins which may not be closed properly.
-;(function ( $, window, document, undefined ) {
+(function ($, window, document, undefined) {
 
-		// Create the defaults once
-		var pluginName = "maskedInput",
-			defaults = {
-				propertyName: "value"
-			};
+    // Create the defaults once
+    var pluginName = "maskedInput",
+        defaults = {
+            propertyName: "value"
+        };
 
-		// The actual plugin constructor
-		function MaskedInput ( element, options ) {
-			this.element = element;
-			this.settings = $.extend( {}, defaults, options );
-			this._defaults = defaults;
-			this._name = pluginName;
-			this.init();
-		}
+    // The actual plugin constructor
+    function MaskedInput(element, options) {
+        this.element = element;
+        this.settings = $.extend({}, defaults, options);
+        this._defaults = defaults;
+        this._name = pluginName;
+        this.init();
+    }
 
-		// Avoid MaskedInput.prototype conflicts
-		$.extend(MaskedInput.prototype, {
-			init: function () {
-				console.log("Initializing simpli masked input.");
-			}
-		});
+    // Avoid MaskedInput.prototype conflicts
+    $.extend(MaskedInput.prototype, {
+        init: function () {
+            console.log("Initializing simpli masked input.");
+        }
+    });
 
-		// A really lightweight plugin wrapper around the constructor,
-		// preventing against multiple instantiations
-		$.fn[ pluginName ] = function ( options ) {
-			this.each(function() {
-				if ( !$.data( this, "simpli" + pluginName ) ) {
-					$.data( this, "simpli" + pluginName, new MaskedInput( this, options ) );
-				}
-			});
+    // A really lightweight plugin wrapper around the constructor,
+    // preventing against multiple instantiations
+    $.fn[pluginName] = function (options) {
+        this.each(function () {
+            if (!$.data(this, "simpli" + pluginName)) {
+                $.data(this, "simpli" + pluginName, new MaskedInput(this, options));
+            }
+        });
 
-			// chain jQuery functions
-			return this;
-		};
+        // chain jQuery functions
+        return this;
+    };
 
-})( jQuery, window, document );
+})(jQuery, window, document);
